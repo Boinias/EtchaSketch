4b61ee826db057562a9dcf0ab2e4f18c1f385848
Jack Francis <jack.francis1100@gmail.com>
2023-02-13 20:09:07 +0000

    Working, but need to make user alter res input

--- EAS.js
+++ EAS.js
@@ -1,5 +1,49 @@
+//Reset
+
+function promptFunc () {
+  var squares = document.getElementsByClassName('square');
+
+  for (var i = 0; i < squares.length; i++) {
+    var element = squares[i];
+    element.style.background = 'white';
+  }
+}
+
+document.getElementById('reset').addEventListener('click', promptFunc);
+
+
+//Choose your res
+
+let resPrompt = 300
+//function resInput () {
+   //let resPrompt = prompt("Choose your res")
+   //resPrompt
+  //ive disabled the above and just made the value 5 for testing purposes
+//}
+
+
 //I've made 4 squares manually for now. I've inputted the width and successfully assigned each square to the class "square"
 
+/*
+var makeSquare = document.createDocumentFragment();
+for(var i=0; i < (resPrompt*resPrompt); i++){
+   var newSquare = document.createElement('div');
+   newSquare.className = 'square';
+   square.style.width = `${500/resprompt}px`;
+   square.setAttribute("class", "square");
+   makeSquare.appendChild(newSquare);
+}*/
+
+let canvas = document.getElementById('canvas');
+    for (let i = 0; i <= (resPrompt*resPrompt); i++) {
+        let makeSquare = document.createElement('div');
+      makeSquare.style.width = `${500/resPrompt}px`;
+      makeSquare.style.height = `${500/resPrompt}px`;
+      makeSquare.setAttribute("class", "square");
+    document.getElementById('canvas').appendChild(makeSquare);
+    }
+
+/*
 const square = document.createElement("div");
 square.style.width = "250px";
 square.setAttribute("class", "square");
@@ -21,17 +65,14 @@ document.getElementById("canvas").appendChild(square);
 document.getElementById("canvas").appendChild(square1);
 document.getElementById("canvas").appendChild(square2);
 document.getElementById("canvas").appendChild(square3);
+*/
 
-
-
-
-// Below I've tried to make each square change colour upon mouseover but only "const = square" (the first square) does so. Please help!
-
+// Mouseover
 
 const allSquares = document.querySelectorAll('.square')
 allSquares.forEach(function(e){
   e.addEventListener('mouseover', function (e) {
      e.target.style.background = "black";
-      //need to make this a toggle thing not just a change colour thing
+    //need to make this a toggle thing not just a change colour thing
   });
 });
